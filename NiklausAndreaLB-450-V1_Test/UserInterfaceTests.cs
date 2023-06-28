ba2b9644872e08a518f43c91cf4abd4aea35e327
IronEsther <andreaniklaus@hotmail.com>
2023-06-28 20:02:55 +0200

    aeuifkjn

--- NiklausAndreaLB-450-V1_Test/UserInterfaceTests.cs
+++ NiklausAndreaLB-450-V1_Test/UserInterfaceTests.cs
@@ -7,95 +7,120 @@ using NUnit.Framework;
 using System.IO;
 using NiklausAndreaLB_450_V1;
 
-namespace NiklausAndreaLB_450_V1_Test
+namespace NiklausAndreaLB_450_V1.Tests
 {
     [TestFixture]
     public class UserInterfaceTests
     {
-        [Test]
-        public void Run_EncryptPasswordOption_CallsEncryptPasswordMethod()
+        private UserInterface userInterface;
+        private PasswordManagerMock passwordManagerMock;
+
+        [SetUp]
+        public void Setup()
         {
-            // Arrange
-            StubPasswordManager passwordManagerStub = new StubPasswordManager();
-            UserInterface userInterface = new UserInterface(passwordManagerStub);
+            passwordManagerMock = new PasswordManagerMock();
+            userInterface = new UserInterface(passwordManagerMock);
+        }
 
-            // Act
-            userInterface.Run("1");
-
-            // Assert
-            Assert.IsTrue(passwordManagerStub.EncryptPasswordCalled);
+        [TearDown]
+        public void Cleanup()
+        {
+            passwordManagerMock.Reset();
         }
 
         [Test]
-        public void Run_DecryptPasswordOption_CallsDecryptPasswordMethod()
+        public void Run_EncryptOption_CallsEncryptPassword()
         {
             // Arrange
-            StubPasswordManager passwordManagerStub = new StubPasswordManager();
-            UserInterface userInterface = new UserInterface(passwordManagerStub);
+            passwordManagerMock.ExpectEncryptPassword("password123");
 
             // Act
-            userInterface.Run("2");
-
-            // Assert
-            Assert.IsTrue(passwordManagerStub.DecryptPasswordCalled);
-        }
-
-        [Test]
-        public void Run_EvaluatePasswordOption_CallsEvaluatePasswordStrengthMethod()
-        {
-            // Arrange
-            StubPasswordManager passwordManagerStub = new StubPasswordManager();
-            UserInterface userInterface = new UserInterface(passwordManagerStub);
-
-            // Act
-            userInterface.Run("3");
-
-            // Assert
-            Assert.IsTrue(passwordManagerStub.EvaluatePasswordStrengthCalled);
-        }
-
-        [Test]
-        public void Run_InvalidOption_DisplaysErrorMessage()
-        {
-            // Arrange
-            StubPasswordManager passwordManagerStub = new StubPasswordManager();
-            UserInterface userInterface = new UserInterface(passwordManagerStub);
-            string expectedOutput = "Ungültige Eingabe. Bitte wählen Sie eine der angegebenen Optionen.";
-
-            // Act
-            using (StringWriter sw = new StringWriter())
+            using (StringReader input = new StringReader("1\npassword123\n4"))
             {
-                Console.SetOut(sw);
-
-                userInterface.Run("invalid");
-
-                string output = sw.ToString().Trim();
-
-                // Assert
-                Assert.AreEqual(expectedOutput, output);
+                Console.SetIn(input);
+                userInterface.Run();
             }
+
+            // Assert
+            passwordManagerMock.ExpectEncryptPassword("password123");
         }
 
-        // Stub-Klasse für den PasswordManager
-        public class StubPasswordManager : IPasswordManager
+        [Test]
+        public void Run_DecryptOption_CallsDecryptPassword()
         {
-            public bool EncryptPasswordCalled { get; private set; }
-            public bool DecryptPasswordCalled { get; private set; }
-            public bool EvaluatePasswordStrengthCalled { get; private set; }
+            // Arrange
+            passwordManagerMock.ExpectDecryptPassword("encrypted123");
+
+            // Act
+            using (StringReader input = new StringReader("2\nencrypted123\n4"))
+            {
+                Console.SetIn(input);
+                userInterface.Run();
+            }
+
+            // Assert
+            passwordManagerMock.ExpectDecryptPassword("encrypted123");
+        }
+
+        [Test]
+        public void Run_EvaluateOption_CallsEvaluatePasswordStrength()
+        {
+            // Arrange
+            passwordManagerMock.ExpectEvaluatePasswordStrength("password123");
+
+            // Act
+            using (StringReader input = new StringReader("3\npassword123\n4"))
+            {
+                Console.SetIn(input);
+                userInterface.Run();
+            }
+
+            // Assert
+            passwordManagerMock.ExpectEvaluatePasswordStrength("password123");
+        }
+
+        // Mock class for PasswordManager
+        public class PasswordManagerMock : IPasswordManager
+        {
+            private string expectedEncryptedPassword;
+            private string expectedDecryptedPassword;
+            private string expectedEvaluatedPassword;
 
             public void EncryptPassword(string password)
             {
-                EncryptPasswordCalled = true;
+                Assert.AreEqual(expectedEncryptedPassword, password);
             }
 
             public void DecryptPassword(string password)
             {
-                DecryptPasswordCalled = true;
+                Assert.AreEqual(expectedDecryptedPassword, password);
             }
 
             public void EvaluatePasswordStrength(string password)
             {
-                EvaluatePasswordStrengthCalled = true;
+                Assert.AreEqual(expectedEvaluatedPassword, password);
+            }
+
+            public void ExpectEncryptPassword(string encryptedPassword)
+            {
+                expectedEncryptedPassword = encryptedPassword;
+            }
+
+            public void ExpectDecryptPassword(string decryptedPassword)
+            {
+                expectedDecryptedPassword = decryptedPassword;
+            }
+
+            public void ExpectEvaluatePasswordStrength(string evaluatedPassword)
+            {
+                expectedEvaluatedPassword = evaluatedPassword;
+            }
+
+            public void Reset()
+            {
+                expectedEncryptedPassword = null;
+                expectedDecryptedPassword = null;
+                expectedEvaluatedPassword = null;
             }
         }
     }
